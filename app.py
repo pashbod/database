5ac20161cca8018e551267eaa1fec29c9b0db492
pashbod <128917611+pashbod@users.noreply.github.com>
2023-12-09 18:47:18 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Конфігурації бази даних
+DATABASE = 'managers.db'
+
+# Функція для отримання підключення до бази даних
+def get_db():
+    db = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+# Створення таблиці користувачів
+def create_table():
+    with get_db() as db:
+        db.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL,
+                password TEXT NOT NULL,
+                age INTEGER,
+                gender TEXT,
+                login_date TEXT
+            )
+        ''')
+        db.commit()
+
+# Реєстрація користувача
+def register_user(email, password, age=None, gender=None):
+    with get_db() as db:
+        now = datetime.now()
+        login_date = now.strftime("%Y-%m-%d %H:%M:%S")
+        db.execute('''
+            INSERT INTO users (email, password, age, gender, login_date)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (email, password, age, gender, login_date))
+        db.commit()
+
+# Головна сторінка
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Обробник для реєстрації
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    password = request.form['password']
+    age = request.form.get('age', None)
+    gender = request.form.get('gender', None)
+
+    register_user(email, password, age, gender)
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    create_table()
+    app.run(debug=True)
